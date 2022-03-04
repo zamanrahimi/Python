23032fcc7010f9720dda6b6b8a9cd08d677d0efd
unknown <zaman.rahimi1368@gmail.com>
2022-03-03 20:08:31 -0800

    How to install psycopg2 on windows 10;

--- create_table.py
+++ create_table.py
@@ -2,12 +2,12 @@
 import mysql.connector
 
 con = mysql.connector.connect(
-host="localhost", user="root", password=""
+host="localhost", user="root", password="", database="python"
 )
 
 cr = con.cursor()
 
-cr.execute("create database python")
+cr.execute("create table emp (id int, name text, phone varchar(100))")
 
 
 
