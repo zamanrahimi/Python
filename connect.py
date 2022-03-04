84756fd6aa1bd67ce17eff495832cc1201a0761c
unknown <zaman.rahimi1368@gmail.com>
2022-03-03 19:11:47 -0800

    Some changes are added to the code to connect python to mysql

--- connect.py
+++ connect.py
@@ -2,8 +2,10 @@
 import mysql.connector
 
 con = mysql.connector.connect(
-host="localhost", user="root",
+host="localhost", user="root", password="", database="python"
 )
+
+cr = con.cursor()
 
 
 
